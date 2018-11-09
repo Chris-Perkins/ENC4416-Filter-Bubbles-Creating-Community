a03336153602d1680ea426b187c9b7c9878cae59
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2018-11-09 18:33:31 -0500

    Separate main screen into info container and button container

---
+++ src/components/InfoBlock.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+
+const titleContainerStyle = {
+    textAlign: "center" as "center",
+    marginLeft: "2.5%",
+    marginRight: "2.5%",
+    marginTop: "7.0%",
+};
+
+const titleTextStyle = {
+    fontSize: "12vh",
+    fontWeight: "bold" as "bold"
+};
+
+const subtitleTextStyle = {
+    fontSize: "6vh"
+};
+
+const infoTextStyle = {
+    fontSize: "4vh",
+    textAlign: "left" as "left",
+    marginLeft: "5.0%",
+    marginRight: "5.0%",
+    marginTop: "2.0%"
+};
+
+export class InfoBlock extends React.Component {
+    render() {
+        return (
+            <div>
+                <div style={titleContainerStyle}>
+                    <span style={titleTextStyle}>
+                        Filter Bubbles
+                    </span>
+                    <br />
+                    <span style={subtitleTextStyle}>
+                        Friend or Foe?
+                    </span>
+                </div>
+                <p style={infoTextStyle}>
+                    Filter bubbles are used by today's largest tech companies including Facebook, Google, and Twitter. 
+                    However, many are divided on their potential use.
+                </p>
+            </div>
+        );
+    }
+}
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,18 +1,12 @@
 import * as React from "react";
-import RF from "react-native-responsive-fontsize";
-import {
-    Jumbotron,
-    Container,
-} from "reactstrap";
-import { Quiz } from "./Quiz";
+import { InfoBlock } from "./InfoBlock";
+import { HomeButtons } from "./HomeButtons";
 
 const pageStyle = {
     marginTop:"2.5%",
     marginLeft:"2.5%",
     width: "95%",
     minHeight:"95%",
-    backgroundColor:"rgba(200,200,200,0.2)",
-    
     position:"absolute" as "absolute"
 };
 
@@ -21,7 +15,8 @@ export class Home extends React.Component {
     render() {
         return (
             <div style={pageStyle}>
-                <Quiz />
+                <InfoBlock />
+                <HomeButtons />
             </div>
         );
     }
