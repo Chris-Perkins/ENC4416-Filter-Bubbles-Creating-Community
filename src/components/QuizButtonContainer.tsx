fbc5cb5d973f27167554352425a8d07b64cc5f57
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2018-11-13 14:09:31 -0500

    Minor button logic change to allow for futher button changes in the future

--- src/components/QuizButtonContainer.tsx
+++ src/components/QuizButtonContainer.tsx
@@ -3,14 +3,14 @@ import Button from "@material-ui/core/Button";
 
 const buttonContainerStyle = {
     textAlign: "center" as "center",
-    marginLeft: "25%",
-    marginRight: "25%",
+    marginLeft: "2.5%",
+    marginRight: "2.5%",
     marginTop: "4.0%",
     height: "7.5vh"
 };
 
 const individualButtonStyle = {
-    width: "100%",
+    width: "50%",
     height: "100%",
     padding: "0%",
     fontSize: "2.5vh"
